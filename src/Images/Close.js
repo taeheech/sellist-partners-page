728c62731544f683468671f59ed632ef9e136208
Taehee <taehee@song-eun-us-MacBook-Pro-2.local>
2020-08-21 11:23:41 +0900

    초기세팅

---
+++ src/Images/Close.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Close() {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M30 19.2L28.8 18L24 22.8L19.2 18L18 19.2L22.8 24L18 28.8L19.2 30L24 25.2L28.8 30L30 28.8L25.2 24L30 19.2Z"
+        fill="#E64A19"
+      />
+    </svg>
+  );
+}
+
+export default Close;
---
+++ src/Images/IconRight.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function IconRight() {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20.764 26.6795L17.207 23.2602L16 24.4204L20.764 29L31 19.1603L29.793 18L20.764 26.6795Z"
+        fill="#00897B"
+      />
+    </svg>
+  );
+}
+
+export default IconRight;
