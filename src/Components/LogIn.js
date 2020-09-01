6b5c8e87c1bbf147a5084890d6ae1dcf81a6bfff
7p3m1k <kor0885@naver.com>
2020-09-01 18:13:11 +0900

    asd

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -20,6 +20,7 @@ function LogIn(props) {
   const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
   function inputLogin(e) {
+    //
     const { name, value } = e.target;
     setLogInState((prevState) => ({
       ...prevState,
